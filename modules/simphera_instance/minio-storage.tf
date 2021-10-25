9e549c09564557e2d42f6648ef67dfedd5bda82c
Dirk Stichling <dstichling@dspace.de>
2021-10-25 14:22:45 +0200

    feat: some outputs added

--- modules/simphera_instance/minio-storage.tf
+++ modules/simphera_instance/minio-storage.tf
@@ -12,8 +12,12 @@ resource "azurerm_resource_group" "minio-storage" {
   }
 }
 
+locals {
+  storageaccountname = substr(replace(join("", [var.name, var.infrastructurename]), "-", ""), 0, 24)
+}
+
 resource "azurerm_storage_account" "minio_storage_account" {
-  name                     = substr(replace(join("", [var.name, var.infrastructurename]), "-", ""), 0, 24)
+  name                     = local.storageaccountname
   resource_group_name      = azurerm_resource_group.minio-storage.name
   location                 = azurerm_resource_group.minio-storage.location
   account_tier             = "Standard"
@@ -28,4 +32,13 @@ resource "azurerm_storage_account" "minio_storage_account" {
       tags
     ]
   }
+}
+
+output "minio_storage_username" {
+  value = local.storageaccountname
+}
+
+output "minio_storage_password" {
+  value     = azurerm_storage_account.minio_storage_account.primary_access_key
+  sensitive = true
 }
