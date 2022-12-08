a0fad0075f64fcd448eaa147d4567db98aedab72
Mario Gluhakovic <mgluhakovic@dspace.hr>
2022-12-08 08:46:31 +0100

    Change extension count satements

--- license-server.tf
+++ license-server.tf
@@ -148,7 +148,7 @@ resource "azurerm_virtual_machine_extension" "azureDiskEncryption" {
 
 # To check the type_handler_version for actuality use `az vm extension image list-versions -l westeurope -p "Microsoft.Azure.Security" -n "IaaSAntimalware"`
 resource "azurerm_virtual_machine_extension" "iaaSAntimalware" {
-  count                = var.licenseServerIaaSAntimalware ? 1 : 0
+  count                = var.licenseServer && var.licenseServerIaaSAntimalware ? 1 : 0
   name                 = "IaaSAntimalware"
   virtual_machine_id   = azurerm_windows_virtual_machine.license-server[0].id
   publisher            = "Microsoft.Azure.Security"
@@ -173,7 +173,7 @@ resource "azurerm_virtual_machine_extension" "iaaSAntimalware" {
 
 # To check the type_handler_version for actuality use `az vm extension image list-versions -l westeurope -p "Microsoft.EnterpriseCloud.Monitoring" -n "MicrosoftMonitoringAgent"`
 resource "azurerm_virtual_machine_extension" "microsoftMonitoringAgent" {
-  count                = var.licenseServerMicrosoftMonitoringAgent ? 1 : 0
+  count                = var.licenseServer && var.licenseServerMicrosoftMonitoringAgent && var.logAnalyticsWorkspaceName != "" ? 1 : 0
   name                 = "MicrosoftMonitoringAgent"
   virtual_machine_id   = azurerm_windows_virtual_machine.license-server[0].id
   publisher            = "Microsoft.EnterpriseCloud.Monitoring"
