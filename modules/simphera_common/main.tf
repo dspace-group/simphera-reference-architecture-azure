6b6e5cb7e54e2b290006f59ed7ff9bb466a42179
Philipp Korth <philipp.korth.dev@mail.de>
2023-01-23 08:28:22 +0100

    Cleans up provider config

--- modules/simphera_common/main.tf
+++ modules/simphera_common/main.tf
@@ -1,21 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.27.0"
-    }
-    local = {
-      version = "2.2.3"
-    }
-  }
-}
-
-provider "azurerm" {
-  subscription_id = var.subscriptionId
-  environment     = var.environment
-  features {}
 }
 
 locals {
