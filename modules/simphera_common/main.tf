210806aa66f24d9d32e0ee5377e00ca580e6b070
Philipp Korth <philipp.korth.dev@mail.de>
2023-01-23 08:19:17 +0100

    Moves fies into new module

---
+++ modules/simphera_common/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.27.0"
+    }
+    local = {
+      version = "2.2.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscriptionId
+  environment     = var.environment
+  features {}
+}
+
+locals {
+  log_analytics_enabled = var.logAnalyticsWorkspaceName != "" ? true : false
+  license_server_secret = var.licenseServer ? jsondecode(data.azurerm_key_vault_secret.license_server_secret[0].value) : { username = "", password = "" }
+}
