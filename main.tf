307106c4378fcc4e4f8cdf31d26b7fd57e941fdd
Mario Gluhakovic <mgluhakovic@dspace.hr>
2023-01-27 15:03:02 +0100

    feat: remove secrets fetch

--- main.tf
+++ main.tf
@@ -20,4 +20,5 @@ provider "azurerm" {
 
 locals {
   log_analytics_enabled = var.logAnalyticsWorkspaceName != "" ? true : false
+  license_server_secret = var.licenseServer ? jsondecode(data.azurerm_key_vault_secret.license_server_secret[0].value) : { username = "", password = "" }
 }
