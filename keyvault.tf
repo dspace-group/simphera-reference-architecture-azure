9d3259cdc8e401a6bba41acb1b75121c72d59e30
Mario Gluhakovic <mgluhakovic@dspace.hr>
2023-01-27 14:51:53 +0100

    feat: remove secrets fetch

--- keyvault.tf
+++ keyvault.tf
@@ -1,19 +1,14 @@
 data "azurerm_client_config" "current" {}
 
-locals {
-  keyvaultnametest = "MarioKeyVaultTest"
-  keyvaultresourcenametest = "MarioKeyVaultRG"
-}
-
-resource "azurerm_resource_group" "key-vault-rg-test" {
-  name     = local.keyvaultresourcenametest
+resource "azurerm_resource_group" "key-vault-rg" {
+  name     = "${var.keyVault}-rg"
   location = var.location
 }
 
-resource "azurerm_key_vault" "simphera-key-vault-test" {
-  name                        = local.keyvaultnametest
-  location                    = azurerm_resource_group.key-vault-rg-test.location
-  resource_group_name         = azurerm_resource_group.key-vault-rg-test.name
+resource "azurerm_key_vault" "simphera-key-vault" {
+  name                        = var.keyVault
+  location                    = azurerm_resource_group.key-vault-rg.location
+  resource_group_name         = azurerm_resource_group.key-vault-rg.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
@@ -68,14 +63,14 @@ resource "azurerm_key_vault" "simphera-key-vault-test" {
   network_acls {
     bypass = "AzureServices"
     default_action = "Deny"
-    ip_rules = ["31.147.185.194"]
+    ip_rules = var.keyVaultAuthorizedIpRanges
     virtual_network_subnet_ids =[]
   }
 }
 
-resource "azurerm_key_vault_key" "azure-disk-encryption-test" {
+resource "azurerm_key_vault_key" "azure-disk-encryption" {
   name         = "AzureDiskEncryption"
-  key_vault_id = azurerm_key_vault.simphera-key-vault-test.id
+  key_vault_id = azurerm_key_vault.simphera-key-vault.id
   key_type     = "RSA"
   key_size     = 2048
 
@@ -95,10 +90,11 @@ resource "random_password" "license-server-rnd-pass" {
   override_special = "!#$%&*-_=+:?"
 }
 
-resource "azurerm_key_vault_secret" "licenseserver-test" {
+resource "azurerm_key_vault_secret" "license_server_secret" {
+  count        = var.licenseServer ? 1 : 0
   name         = "licenseserver"
   value        = jsonencode({"username" : "lcserveruser","password" : "${random_password.license-server-rnd-pass.result}"})
-  key_vault_id = azurerm_key_vault.simphera-key-vault-test.id
+  key_vault_id = azurerm_key_vault.simphera-key-vault.id
 }
 
 resource "random_password" "postgresql-rnd-pass" {
@@ -107,16 +103,16 @@ resource "random_password" "postgresql-rnd-pass" {
   override_special = "!#$%&*-_=+:?"
 }
 
-resource "azurerm_key_vault_secret" "postgresqlCredentials-test" {
+resource "azurerm_key_vault_secret" "postgresqlCredentials" {
   name         = "postgresqlCredentials"
   value        = jsonencode({"postgresql_username" : "dbuser","postgresql_password" : "${random_password.postgresql-rnd-pass.result}"})
-  key_vault_id = azurerm_key_vault.simphera-key-vault-test.id
+  key_vault_id = azurerm_key_vault.simphera-key-vault.id
 }
 
 resource "azurerm_private_endpoint" "keyvault-private-endpoint" {
   name                = "keyvault-private-endpoint"
   location            = var.location
-  resource_group_name = local.keyvaultresourcenametest
+  resource_group_name = azurerm_resource_group.key-vault-rg.name
   subnet_id           = azurerm_subnet.paas-services-subnet.id
 
   private_dns_zone_group {
@@ -126,7 +122,7 @@ resource "azurerm_private_endpoint" "keyvault-private-endpoint" {
 
   private_service_connection {
     name                           = "simphera-keyvault"
-    private_connection_resource_id = azurerm_key_vault.simphera-key-vault-test.id
+    private_connection_resource_id = azurerm_key_vault.simphera-key-vault.id
     is_manual_connection           = false
     subresource_names              = ["vault"]
   }
