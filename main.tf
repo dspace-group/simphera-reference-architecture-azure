d0663067fd93d0ab028d7e55f66615607cfa63e3
Philipp Korth <philipp.korth.dev@mail.de>
2023-03-06 10:43:05 +0100

    Adds module usage

--- main.tf
+++ main.tf
@@ -20,3 +20,33 @@ provider "azurerm" {
   environment     = var.environment
   features {}
 }
+
+
+module "simphera_base" {
+  source = "./modules/simphera_base"
+
+  infrastructurename                       = var.infrastructurename
+  tags                                     = var.tags
+  linuxNodeSize                            = var.linuxNodeSize
+  linuxNodeCountMin                        = var.linuxNodeCountMin
+  linuxExecutionNodeSize                   = var.linuxExecutionNodeSize
+  linuxExecutionNodeCountMax               = var.linuxExecutionNodeCountMax
+  linuxExecutionNodeDeallocate             = var.linuxExecutionNodeDeallocate
+  gpuNodePool                              = var.gpuNodePool
+  gpuNodeCountMin                          = var.gpuNodeCountMin
+  gpuNodeCountMax                          = var.gpuNodeCountMax
+  gpuNodeSize                              = var.gpuNodeSize
+  gpuNodeDeallocate                        = var.gpuNodeDeallocate
+  ssh_public_key_path                      = var.ssh_public_key_path
+  licenseServer                            = var.licenseServer
+  licenseServerIaaSAntimalware             = var.licenseServerIaaSAntimalware
+  licenseServerMicrosoftMonitoringAgent    = var.licenseServerMicrosoftMonitoringAgent
+  licenseServerMicrosoftGuestConfiguration = var.licenseServerMicrosoftGuestConfiguration
+  logAnalyticsWorkspaceName                = var.logAnalyticsWorkspaceName
+  logAnalyticsWorkspaceResourceGroupName   = var.logAnalyticsWorkspaceResourceGroupName
+  kubernetesVersion                        = var.kubernetesVersion
+  keyVaultAuthorizedIpRanges               = var.keyVaultAuthorizedIpRanges
+  simpheraInstances                        = var.simpheraInstances
+  apiServerAuthorizedIpRanges              = var.apiServerAuthorizedIpRanges
+
+}
