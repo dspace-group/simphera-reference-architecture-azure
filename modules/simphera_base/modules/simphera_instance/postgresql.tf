365d72993db2caa1c1bbcb31f82a0943e1652571
Mario Gluhaković <115137478+mariogluhakovic@users.noreply.github.com>
2023-04-24 09:38:08 +0200

    Fix complexity of password (#86)
    * fix: fix complexity of password
    
    * fix: run formater

--- modules/simphera_base/modules/simphera_instance/postgresql.tf
+++ modules/simphera_base/modules/simphera_instance/postgresql.tf
@@ -8,6 +8,10 @@ resource "random_password" "postgresql-password" {
   length           = 16
   special          = true
   override_special = "!#$%&*-_+:?"
+  min_lower        = 2
+  min_upper        = 2
+  min_special      = 2
+  min_numeric      = 2
 }
 
 resource "azurerm_key_vault_secret" "postgresql-credentials" {
