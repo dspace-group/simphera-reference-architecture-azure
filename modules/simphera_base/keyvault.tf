03ba887fb8eedc38ae687af978036ed5abb204e9
Philipp Korth <34297115+PhilippKorth@users.noreply.github.com>
2023-05-03 13:25:52 +0200

    Removes some special characters (#87)
    * Removes some special characters
    
    * Removes some special characters

--- modules/simphera_base/keyvault.tf
+++ modules/simphera_base/keyvault.tf
@@ -72,7 +72,7 @@ resource "azurerm_key_vault_key" "azure-disk-encryption" {
 resource "random_password" "license-server-password" {
   length           = 16
   special          = true
-  override_special = "!#$%&*-_=+:?"
+  override_special = "!#%&*-_+?"
   min_lower        = 2
   min_upper        = 2
   min_special      = 2
