e8ce9863cda4b452dccac8c672b1ea9b21be7b65
Simon Schwichtenberg <SSchwichtenberg@dspace.de>
2023-06-30 09:30:14 +0200

    precommit hook

--- hooks/validate_tfvars.sh
+++ hooks/validate_tfvars.sh
@@ -3,6 +3,7 @@
 #SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # run terraform apply and redirect stderr to stdout
 cd /src
+terraform init
 OUTPUT=`terraform apply --var-file="terraform.tfvars.example" -input=false 2>&1 >/dev/null`
 echo $OUTPUT
 # ignore az login
